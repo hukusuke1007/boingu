5086505e00de43bb0a6ca938fad5f7f542050b5b
Shohei Nakagawa <hukusuke1007@gmail.com>
2018-06-17 23:08:29 +0900

    u

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ const host =
   "localhost"
 
 // `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
-const basePath = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/nuxt-ts-plain/' : '/'
+const basePath = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/boingu/' : '/'
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
     base: basePath
